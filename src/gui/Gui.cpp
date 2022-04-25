f7102fb259a95c37878838dee53df28db71dd656
phlawlessDevelopment <phalyce@outlook.com>
2022-04-25 23:07:01 +0100

    started imgui docking stuff

--- src/gui/Gui.cpp
+++ src/gui/Gui.cpp
@@ -5,6 +5,13 @@ GUI::GUI(GLFWwindow* window, float* translationA, float* translationB)
 	m_window(window)
 {
 	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 	ImGui_ImplGlfw_InitForOpenGL(m_window,true);
 	ImGui_ImplOpenGL3_Init();
 	ImGui::StyleColorsDark();	
@@ -22,9 +29,20 @@ void GUI::Render()
     	ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
+		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->Pos);
+		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::SetNextWindowViewport(viewport->ID);
+		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
+		ImGui::Begin("win");
 		ImGui::SliderFloat3("Translation A",m_translationA,-2.0f,2.0f);
-
+		ImGui::End();
+		ImGui::Begin("win 2");
+		ImGui::End();
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
--- src/gui/Gui.h
+++ src/gui/Gui.h
@@ -2,6 +2,7 @@
 #include "vendor/imgui/imgui.h"
 #include "vendor/imgui/imgui_impl_glfw.h"
 #include "vendor/imgui/imgui_impl_opengl3.h"
+
 class GUI
 {
     private:
